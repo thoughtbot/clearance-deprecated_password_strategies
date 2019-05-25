ae7c1df534cca4c177102845fc90736ac67eb226
Aditya Prakash <aditya.prakash132@gmail.com>
2019-05-26 01:58:34 +0530

    Fix require path

--- lib/clearance-deprecated_password_strategies.rb
+++ lib/clearance-deprecated_password_strategies.rb
@@ -1,4 +1,4 @@
-require "clearance/password_strategies/deprecated/version"
-require "clearance/password_strategies/deprecated/blowfish"
-require "clearance/password_strategies/deprecated/sha1"
-require "clearance/password_strategies/deprecated/bcrypt_migration_from_sha1"
+require "clearance/password_strategies/version"
+require "clearance/password_strategies/blowfish"
+require "clearance/password_strategies/sha1"
+require "clearance/password_strategies/bcrypt_migration_from_sha1"
